fde9e745f0287179f1d8533775de6d4d49ccefb5
SpaGheTti <Solipeyman1@gmail.com>
2025-01-12 00:21:45 +0330

    0.01

--- Assets/ItemIdentity.cs
+++ Assets/ItemIdentity.cs
@@ -1,10 +1,6 @@
 using UnityEngine;
 
-/// <summary>
-/// Attach this script to any spawned item so we can track its original prefab.
-/// </summary>
 public class ItemIdentity : MonoBehaviour
 {
-    // Which prefab was used to create this item
-    public GameObject originalPrefab;
+    [HideInInspector]public GameObject originalPrefab;
 }
